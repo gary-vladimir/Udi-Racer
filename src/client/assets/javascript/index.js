2406e4f06b8e3a83344eef598c759cf76a35cc73
Gary Nuñez <yragi.nunez@gmail.com>
2021-03-30 14:56:58 -0600

    accelerate function done

--- src/client/assets/javascript/index.js
+++ src/client/assets/javascript/index.js
@@ -376,4 +376,14 @@ function accelerate(id) {
     // POST request to `${SERVER}/api/races/${id}/accelerate`
     // options parameter provided as defaultFetchOpts
     // no body or datatype needed for this request
+    return fetch(`${SERVER}/api/races/${id}/accelerate`, {
+        method: 'POST',
+        ...defaultFetchOpts(),
+    })
+        .then((res) => res.json())
+        .then((data) => {
+            console.log(data);
+            return data;
+        })
+        .catch((err) => console.log('Problem with getRace request::', err));
 }
